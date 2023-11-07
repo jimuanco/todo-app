0e1648f5b37d576c2c52f49fc9fdffc89014ce34
Jimoo An <jmdh1004@gmail.com>
2023-11-07 20:18:56 +0900

    :sparkles: User Layers 구현

---
+++ src/main/java/myproject/todoapp/service/UserService.java
@@ -0,0 +1,31 @@
+package myproject.todoapp.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import myproject.todoapp.model.UserEntity;
+import myproject.todoapp.persistence.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserEntity create(final UserEntity userEntity) {
+        if (userEntity == null || userEntity.getUsername() == null) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        final String username = userEntity.getUsername();
+        if (userRepository.existsByUsername(username)) {
+            log.warn("Username already exists {}", username);
+            throw new RuntimeException("Username already exists");
+        }
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity getByCredentials(final String username, final String password) {
+        return userRepository.findByUsernameAndPassword(username, password);
+    }
+}
