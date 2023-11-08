63bc291d262410e1785c95c47d2135296ad64c75
Jimoo An <jmdh1004@gmail.com>
2023-11-08 16:07:45 +0900

    :wrench: SecurityConfiguration 추가

---
+++ src/main/java/myproject/todoapp/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package myproject.todoapp.config;
+
+import lombok.RequiredArgsConstructor;
+import myproject.todoapp.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req ->
+                            req.requestMatchers("/", "/auth/**")
+                                    .permitAll()
+                                    .anyRequest()
+                                    .authenticated())
+                .addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+
+        return http.build();
+    }
+}
